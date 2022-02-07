5f1be069a8b54b1e9c61a31b67084b8c38f867e9
Anson <awong30@stuy.edu>
2022-02-07 11:48:06 -0500

    started partialSum bc couldnt do toWords couldn't do partialSum

--- Classwork/Classwork.java
+++ Classwork/Classwork.java
@@ -2,4 +2,27 @@ public class Classwork{
   public static String toWords(int n){
 
   }
+  // public boolean partialSum(int[] arr, int target){
+  //   partialSum(0, arr, target);
+  // }
+//   public static void printNoDoubleLetterWords(int length,String word,char[]letters){
+//   if (length > 0){
+//     for (int i = 0; i < letters.length; i++){
+//       if (word.length() == 0 || word.charAt(word.length() - 1) != letters[i]){
+//         printNoDoubleLetterWords(length - 1, word + letters[i], letters);
+//       }
+//     }
+//   }else{
+//     System.out.println(word);
+//   }
+// }
+  public boolean partialSum(start, int[] arr,targetValue){
+    if (start == arr.length){
+      return false;
+    }else{
+      for (int i = 0; i < arr.length - start; i++){
+        if (arr[start] )
+      }
+    }
+  }
 }
