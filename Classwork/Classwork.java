06d6bcdb638f0acfdefd04cbb03c8eab84e8787e
Anson <awong30@stuy.edu>
2022-02-08 11:46:20 -0500

    put booleans but not sure if right and not sure how to do rest of code

--- Classwork/Classwork.java
+++ Classwork/Classwork.java
@@ -1,28 +1,18 @@
 public class Classwork{
-  public static String toWords(int n){
-
-  }
-  // public boolean partialSum(int[] arr, int target){
-  //   partialSum(0, arr, target);
-  // }
-//   public static void printNoDoubleLetterWords(int length,String word,char[]letters){
-//   if (length > 0){
-//     for (int i = 0; i < letters.length; i++){
-//       if (word.length() == 0 || word.charAt(word.length() - 1) != letters[i]){
-//         printNoDoubleLetterWords(length - 1, word + letters[i], letters);
-//       }
-//     }
-//   }else{
-//     System.out.println(word);
-//   }
-// }
-  public boolean partialSum(start, int[] arr,targetValue){
-    if (start == arr.length){
-      return false;
-    }else{
-      for (int i = 0; i < arr.length - start; i++){
-        if (arr[start] )
+  public static boolean partialSum(int start,int[] arr,int targetValue){
+    if (targetValue > 0 || start == arr.length - 1){
+      if (!partialSum()){
+        
       }
     }
+    else if (targetValue == 0){
+      return true;
+    }
+    return false;
+  }
+
+  public static void main (String [] args){
+    int[] arr = {5, 3, 7, 1};
+    System.out.println(partialSum(0, arr, 11));
   }
 }
