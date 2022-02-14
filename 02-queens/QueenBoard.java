966623de17bb982ae8100660768daef0b84fb86e
Anson <awong30@stuy.edu>
2022-02-14 11:28:20 -0500

    did toString maybe didn't do w queens yet

--- 02-queens/QueenBoard.java
+++ 02-queens/QueenBoard.java
@@ -1,5 +1,9 @@
 public class QueenBoard{
   private int[][]board;
+  public static void main (String[] args){
+    QueenBoard board = new QueenBoard(4);
+    System.out.println(board.toString());
+  }
   public QueenBoard(int size){
     board = new int[size][size];
     for(int i = 0; i < size; i++){
@@ -19,42 +23,53 @@ public class QueenBoard{
   *_ Q _ _
   *excludes the characters up to the comment(*)
   */
-  public String toString(){}
+  public String toString(){
+    String ans = "";
+    for (int i = 0; i < board.length; i++){
+      for (int j = 0; j < board.length; j++){
+        if (board[i][j] == -1){
+          ans += "Q ";
+        }else{
+          ans += "_ ";
+        }
+      }ans += '\n';
+    }return ans;
+  }
 
   /**
   *@return true when the queen added correctly, false Otherwise
   *@precondition r and c are valid indices of the board array
   *@postcondition the board is only changed when the function returns true
   * in which case the queen is added and all it's threatened positions are incremented
-  */
-  private boolean addQueen(int r, int c){
-
-  }
-
-  /**Remove the queen that was added to r,c
-  *@precondition r and c are valid indices of the board array and there is a queen at position r,c
-  *@postcondition the board is modified to remove that queen and all it's
-  *threatened positions are decremented
-  */
-  private void removeQueen(int r, int c){
-
-  }
-
-  /**Find the first solution configuration possible for this size board. Start by placing
-  *  the 1st queen in the top left corner, and each new queen in the next ROW. When backtracking
-  *  move the previous queen to the next valid space. This means everyone will generate the same
-  *  first solution.
-  *@postcondition: the board remains in a solved state.
-  *@return false when the board is not solveable and leaves the board filled with zeros;
-  *        returns true when the board is solveable, and leaves the board in a solved state
-  *@throws IllegalStateException when the board starts with any non-zero value (e.g. you solved a 2nd time.)
-  */
-  //solve & countSolutions are wrappers
-  public boolean solve(){}
-
-  /**Find all possible solutions to this size board.
-  *@return the number of solutions found, and leaves the board filled with only 0's
-  *@throws IllegalStateException when the board starts with any non-zero value (e.g. you ran solve() before this method)
-  */
-  public int countSolutions(){}
+  // */
+  // private boolean addQueen(int r, int c){
+  //
+  // }
+  //
+  // /**Remove the queen that was added to r,c
+  // *@precondition r and c are valid indices of the board array and there is a queen at position r,c
+  // *@postcondition the board is modified to remove that queen and all it's
+  // *threatened positions are decremented
+  // */
+  // private void removeQueen(int r, int c){
+  //
+  // }
+  //
+  // /**Find the first solution configuration possible for this size board. Start by placing
+  // *  the 1st queen in the top left corner, and each new queen in the next ROW. When backtracking
+  // *  move the previous queen to the next valid space. This means everyone will generate the same
+  // *  first solution.
+  // *@postcondition: the board remains in a solved state.
+  // *@return false when the board is not solveable and leaves the board filled with zeros;
+  // *        returns true when the board is solveable, and leaves the board in a solved state
+  // *@throws IllegalStateException when the board starts with any non-zero value (e.g. you solved a 2nd time.)
+  // */
+  // //solve & countSolutions are wrappers
+  // public boolean solve(){}
+  //
+  // /**Find all possible solutions to this size board.
+  // *@return the number of solutions found, and leaves the board filled with only 0's
+  // *@throws IllegalStateException when the board starts with any non-zero value (e.g. you ran solve() before this method)
+  // */
+  // public int countSolutions(){}
 }
