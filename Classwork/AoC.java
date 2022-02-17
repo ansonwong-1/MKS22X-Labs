0b36b4e96f88e59aa3928472cd666cbc2af9f2cd
Anson <awong30@stuy.edu>
2022-02-17 11:48:19 -0500

    day2

--- Classwork/AoC.java
+++ Classwork/AoC.java
@@ -6,12 +6,31 @@ public class AoC{
       String fileName = args[0];
       File file = new File(fileName);
       Scanner in = new Scanner(file);
-      System.out.println(day1(file));
+      System.out.println(day2(file));
     }catch (FileNotFoundException e){
       System.out.println("ERROR: File not found");
     }
   }
-
+  public static int day2(File file){
+    int count = 0;
+    try{
+      Scanner in = new Scanner(file);
+      int a = in.nextInt();
+      int b = in.nextInt();
+      int c = in.nextInt();
+      while(in.hasNextInt()){
+        int d = in.nextInt();
+        if(a + b + c < b + c + d){
+          count++;
+        }
+        a = b;
+        b = c;
+        c = d;
+      }
+    }catch(FileNotFoundException e){
+      return -1;
+    }return count;
+  }
   public static int day1(File file){
     int total = 0;
     try{
