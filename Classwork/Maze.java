1eeb7fb5ab75ccfba0a3f4171c3b4ae062853115
Anson <awong30@stuy.edu>
2022-02-19 17:18:24 -0500

    had idea but didn't work out

--- Classwork/Maze.java
+++ Classwork/Maze.java
@@ -1,25 +1,32 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+
 public class Maze{
   public static void main (String[] args){
     try{
       String fileName = args[0];
       File file = new File(fileName);
-      System.out.println(mazeToArr());
+      System.out.println(mazeToArr(file));
     }catch (FileNotFoundException e){
       System.out.println("No file found");
     }
   }
 
   public static char[][] mazeToArr(File file){
-    String ans = "";
-    Scanner in = new Scanner(file);
-    while(in.hasNext()){
-      ans += in.next() + '\n';
-    }return ans.toCharArray(ans);
+    Arraylist<String> rows = new Arraylist<String>();
+    try{
+      Scanner in = new Scanner(file);
+      while(in.hasNext()){
+        rows.add(in.next());
+      }int c = (rows.get(0)).length;
+      char[][]maze = new char[rows.size()][c];
+      for (int i = 0; i < rows.size(); i++){
+        maze[i] = (rows.get(i)).toCharArray();
+      }return maze;
+    }catch (FileNotFoundException e){
+      return null;
+    }
   }
-
   public static String toString(char[][] arr){
     String ans = "";
     for(int i = 0; i < arr.length; i++){
