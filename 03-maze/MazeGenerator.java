1782823fb28a9ea7da99d72a4bea47235026ed65
Anson <awong30@stuy.edu>
2022-03-04 15:34:08 -0500

    tried to clean up the not hit walls/out of bounds stuff but still doesnt work

--- 03-maze/MazeGenerator.java
+++ 03-maze/MazeGenerator.java
@@ -10,29 +10,59 @@ public class MazeGenerator{
   public static void generate(char[][]maze, int startrow, int startcol){
     int[][] directions = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
     Random rng = new Random();
-    int[] direction = directions[rng.nextInt(4)];
-    boolean safeToCarve = false;
-    for (int i = 0 ; i < directions.length; i++){
-      int r = startrow + directions[i][0];
-      int c = startcol + directions[i][1];
-      if (r > 0 && c > 0 && r < maze.length - 1 && c < maze[r].length - 1)
-      {int neighbors = 0;
-      if(maze[startrow][startcol] == '#'){
-        for(int j = 0; j < directions.length; j++){
-          int rj = startrow + directions[j][0];
-          int cj = startcol + directions[j][1];
-          if (rj > 0 && cj > 0 && rj < maze.length - 1 && cj < maze[r].length - 1){
-            if (maze[r][c] == ' '){
-              neighbors++;
-            }
+    // int[] direction = directions[rng.nextInt(4)];
+    boolean safeToCarve = true;
+    if (maze[startrow][startcol] == ' '){
+      safeToCarve = false;
+    }if (startrow > maze.length - 1 || startrow <= 0
+    || startcol > maze.length - 1 || startcol <= 0){
+      safeToCarve = false;
+    }else{
+      int neighbors = 0;
+      for(int j = 0; j < directions.length; j++){
+        int r = startrow + directions[j][0];
+        int c = startcol + directions[j][1];
+        if (r > 0 && c > 0 && r < maze.length - 1 && c < maze[r].length - 1){
+          if (maze[r][c] == ' '){
+            neighbors++;
           }
-        }if (neighbors < 2){
-          safeToCarve = true;
         }
-      }if (safeToCarve){
-        maze[startrow + direction[0]][startcol + direction[1]] = ' ';
-      }}
+      }if (neighbors > 2){
+        safeToCarve = false;
+      }
     }
+    if (safeToCarve)
+    {for (int i = 0; i < directions.length; i++){
+      maze[startrow][startcol] = ' ';
+      int[] direction = directions[rng.nextInt(4)];
+    }}String ans = "";
+    for (int i = 0; i < maze.length; i++){
+      for(int j = 0; j < maze[i].length; j++){
+        ans += maze[i][j];
+      }ans += '\n';
+    }System.out.println(ans);
+//     boolean safeToCarve = false;
+// for (int i = 0 ; i < directions.length; i++){
+//   int r = startrow + directions[i][0];
+//   int c = startcol + directions[i][1];
+//   if (r > 0 && c > 0 && r < maze.length - 1 && c < maze[r].length - 1)
+//   {int neighbors = 0;
+//   if(maze[startrow][startcol] == '#'){
+//     for(int j = 0; j < directions.length; j++){
+//       int rj = startrow + directions[j][0];
+//       int cj = startcol + directions[j][1];
+//       if (rj > 0 && cj > 0 && rj < maze.length - 1 && cj < maze[r].length - 1){
+//         if (maze[r][c] == ' '){
+//           neighbors++;
+//         }
+//       }
+//     }if (neighbors < 2){
+//       safeToCarve = true;
+//     }
+//   }if (safeToCarve){
+//     maze[startrow + direction[0]][startcol + direction[1]] = ' ';
+//   }}
+// }
     // int count1;
     // if (maze[row][col] == 'E'){
     //   return count;
