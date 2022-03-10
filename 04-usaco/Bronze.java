bc166fc0281ddbb665d0451caa2049523b1b6eca
Anson <awong30@stuy.edu>
2022-03-10 10:48:03 -0500

    started

---
+++ 04-usaco/Bronze.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+public class Bronze{
+
+  public static long solve(String filename){
+    try{
+      File file = new File(filename);
+      Scanner in = new Scanner(file);
+      int[][] pasture = new int[in.nextInt()][in.nextInt()];
+      for (int i = 0; i < pasture.length; i++){
+        for (int j = 0; j < pasture[i].length; j++){
+
+        }
+      }
+    }catch (FileNotFoundException e){
+      return -1;
+    }return vol;
+  }
+
+
+}
