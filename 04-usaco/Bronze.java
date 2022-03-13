39edea3747abc3a678f6f9facf5d394dd4b6872a
Anson <awong30@stuy.edu>
2022-03-13 03:06:42 -0400

    fixed index out of bounds errors

--- 04-usaco/Bronze.java
+++ 04-usaco/Bronze.java
@@ -1,7 +1,9 @@
 import java.util.*;
 import java.io.*;
 public class Bronze{
-
+  public static void main(String[] args){
+    System.out.println(solve(args[0]));
+  }
   public static long solve(String filename){
     try{
       File file = new File(filename);
@@ -23,7 +25,7 @@ public class Bronze{
       for(int i = 0; i < r; i++){
           for(int j = 0; j < c; j++){
               if(e > pasture[i][j]){
-                d += e - pasture[r][c];
+                d += e - pasture[i][j];
               }
           }
       }return d * 72 * 72;
@@ -35,6 +37,8 @@ public class Bronze{
 
 
   public static void stomp(int[][] pasture, int R_s, int C_s, int D_s){
+    R_s -= 1;
+    C_s -= 1;
     int greatest = pasture[R_s][C_s];
     for (int i = 0; i < 3; i++){
       for(int j = 0; j < 3; j++){
