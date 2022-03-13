dc5b8f62f5da34a6ee0adcad63aa627ffaddc99d
Anson <awong30@stuy.edu>
2022-03-13 18:42:05 -0400

    tried to write out solve but have an error with making the array

--- 04-usaco/Silver.java
+++ 04-usaco/Silver.java
@@ -1,6 +1,9 @@
 import java.io.*;
 import java.util.*;
 public class Silver{
+  public static void main (String[] args){
+    System.out.println(solve(args[0]));
+  }
   public static long solve(String filename){
     try{
       File file = new File(filename);
@@ -8,7 +11,7 @@ public class Silver{
       int n = in.nextInt();
       int m = in.nextInt();
       int t = in.nextInt();
-      int s = 0;
+      long s = 0;
       long [][] pasture = new long[n][m];
       for (int i = 0; i < n; i++){
         String row = in.nextLine();
@@ -20,10 +23,34 @@ public class Silver{
           }
         }
       }
+      pasture[in.nextInt() - 1][in.nextInt() - 1] = 1;
+      ways(pasture, t);
+      s = pasture[in.nextInt() - 1][in.nextInt() - 1];
       return s;
     }catch(FileNotFoundException e){
       return -1;
     }
   }
+  
+  public static void ways(long[][] pasture, int t){
+    int[][] directions = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    for (int i = 0; i < t; i++){
+      for (int j = 0; j < pasture.length; j++){
+        for (int k = 0; k < pasture[j].length; k++){
+          if (pasture[j][k] == 0){
+            for (int l = 0; l < 4; i++){
+              if (inBounds(pasture, j + directions[l][0], k + directions[l][1])){
+                pasture[j][k] += pasture[j + directions[l][0]][k + directions[l][1]];
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 
+  public static boolean inBounds(long[][]pasture, int i, int j){
+    return !(i < 0 || i > pasture.length ||
+    j < 0 || j < pasture[0].length);
+  }
 }
