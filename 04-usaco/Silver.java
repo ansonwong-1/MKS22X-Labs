29d5d77c2dc40f44ba11e70b00fad16fd9999fb7
Anson <awong30@stuy.edu>
2022-03-13 16:51:19 -0400

    fixed variable names & read in pasture

--- 04-usaco/Silver.java
+++ 04-usaco/Silver.java
@@ -5,9 +5,21 @@ public class Silver{
     try{
       File file = new File(filename);
       Scanner in = new Scanner(file);
-      int r = in.nextInt();
-      int c = in.nextInt();
+      int n = in.nextInt();
+      int m = in.nextInt();
+      int t = in.nextInt();
       int s = 0;
+      long [][] pasture = new long[n][m];
+      for (int i = 0; i < n; i++){
+        String row = in.nextLine();
+        for (int j = 0; j < m; j++){
+          if (row.charAt(j) == '*'){
+            pasture[i][j] = -1;
+          }else{
+            pasture[i][j] = 0;
+          }
+        }
+      }
       return s;
     }catch(FileNotFoundException e){
       return -1;
