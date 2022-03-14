1760bfc091495d75095ce2cad29776a018fa8b18
Anson <awong30@stuy.edu>
2022-03-14 01:38:59 -0400

    fixed errors- out of bounds, incorrect operators, changed output of helper, made separate array for helper, added in.nextLine() in solve

--- 04-usaco/Silver.java
+++ 04-usaco/Silver.java
@@ -13,6 +13,7 @@ public class Silver{
       int t = in.nextInt();
       long s = 0;
       long [][] pasture = new long[n][m];
+      in.nextLine();
       for (int i = 0; i < n; i++){
         String row = in.nextLine();
         for (int j = 0; j < m; j++){
@@ -24,33 +25,41 @@ public class Silver{
         }
       }
       pasture[in.nextInt() - 1][in.nextInt() - 1] = 1;
-      ways(pasture, t);
-      s = pasture[in.nextInt() - 1][in.nextInt() - 1];
-      return s;
+      pasture = ways(pasture, t);
+      return pasture[in.nextInt() - 1][in.nextInt() - 1];
     }catch(FileNotFoundException e){
       return -1;
     }
   }
-  
-  public static void ways(long[][] pasture, int t){
+
+
+  public static long[][] ways(long[][] pasture, int t){
     int[][] directions = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-    for (int i = 0; i < t; i++){
-      for (int j = 0; j < pasture.length; j++){
-        for (int k = 0; k < pasture[j].length; k++){
-          if (pasture[j][k] == 0){
-            for (int l = 0; l < 4; i++){
-              if (inBounds(pasture, j + directions[l][0], k + directions[l][1])){
-                pasture[j][k] += pasture[j + directions[l][0]][k + directions[l][1]];
+    while(t > 0){
+      long [][]ans= new long[pasture.length][pasture[0].length];
+      for(int j = 0; j < pasture.length; j++){
+        for(int k = 0; k < pasture[j].length; k++){
+          if(pasture[j][k] == -1){
+            ans[j][k] = -1;
+          }else if(pasture[j][k] == 0){
+            for(int l = 0; l < 4; l++){
+              if(inBounds(pasture, j + directions[l][0], k + directions[l][1])){
+                ans[j][k] += pasture[j + directions[l][0]][k + directions[l][1]];
               }
             }
+          }else if(pasture[j][k] > 0){
+            ans[j][k] = 0;
           }
         }
       }
-    }
+      pasture = ans;
+      t--;
+    }return pasture;
   }
 
   public static boolean inBounds(long[][]pasture, int i, int j){
-    return !(i < 0 || i > pasture.length ||
-    j < 0 || j < pasture[0].length);
+    return !(i < 0 || i > pasture.length - 1||
+    j < 0 || j > pasture[0].length - 1
+    || pasture[i][j] == -1);
   }
 }
