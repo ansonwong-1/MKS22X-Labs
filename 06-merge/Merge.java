df21adbb7687402b63c6a619120c7ef9adec0e55
Anson <awong30@stuy.edu>
2022-03-24 11:39:44 -0400

    restarted- put prat of the left into the merged

--- 06-merge/Merge.java
+++ 06-merge/Merge.java
@@ -1,68 +1,49 @@
 import java.util.*;
 import java.io.*;
 public class Merge{
-
-  public static int[] mergesortH(int[]data){
-    if(data.length > 1){
-      int[] L = new int[data.length / 2];
-      if(data.length % 2 == 1){
-        int[] R2 = new int[data.length / 2 + 1];
-        for(int i = 0; i < data.length; i++){
-          if (i < data.length / 2){
-            L[i] = data[i];
-          }else{
-            R2[i] = data[i];
-          }
-        }mergesort(L);
-        mergesort(R2);
-        return merge(L, R2);
-      }else{
-        int[] R = new int[data.length / 2];
-        for(int i = 0; i < data.length; i++){
-          if (i < data.length / 2){
-            L[i] = data[i];
-          }else{
-            R[i] = data[i];
-          }
-        }
-        mergesort(L);
-        mergesort(R);
-        return merge(L, R);
-      }
-    }return data;
+  public static void main (String[] args){
+    int[] data = {3,51,2,3,1,4,3,6,7};
+    Arrays.sort(data);
+    int[] data1 = {34,-31,-4,-5,-33};
+    Arrays.sort(data1);
+    System.out.println(Arrays.toString(merge(data1, data)));
   }
+
   public static int[] merge(int[] left, int[] right){
     int[] merged = new int[left.length + right.length];
     int ll = 0, lr = 0;
-    for (int i = 0; i < merged.length; i++){
-      if(ll >= left.length){
-        while(i < merged.length){
-          merged[i] = right[ll];
-          ll++;
-          i++;
-        }
-      }else if(lr >= right.length){
-        while(i < merged.length){
-          merged[i] = right[lr];
-          lr++;
-          i++;
-        }
-      }else{
-        if(left[ll] < right[lr]){
+    for(int i = 0; i < merged.length; i++){
+      if (i < left.length){
+        if (left[ll] < right[lr]){
           merged[i] = left[ll];
           ll++;
-        }else{
-          merged[i] = right[lr];
-          lr++;
         }
       }
     }return merged;
   }
 
-public static void mergesort(int[] data){
-  int [] temp  =  mergesortH(data);
-  data = Arrays.copyOf(temp, temp.length);
-}
+// public static void mergesort(int[] data){
+//   int [] temp  =  mergesortH(data);
+//   data = Arrays.copyOf(temp, temp.length);
+// }
+// public static int[] mergesortH(int[]data){
+//   if(data.length > 1){
+//     int l = data.length / 2;
+//     int r = data.length - l;
+//     int[] L = new int[l];
+//     int[] R = new int[r];
+//     for(int i = 0; i < data.length; i++){
+//       if (i < l){
+//         L[i] = data[i];
+//       }else{
+//         R[i - l] = data[i];
+//       }
+//     }
+//     mergesort(L);
+//     mergesort(R);
+//     return merge(L, R);
+//   }return data;
+// }
 //
 // int[] mergesortH(data){
 //   if more than 1 element{
