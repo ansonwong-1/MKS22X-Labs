42e0b77ea3b1e0d8a7535d7d78ccfc7ffbd1d952
Anson <awong30@stuy.edu>
2022-03-28 11:22:31 -0400

    copy pasted

---
+++ 07-mydeque/MyDeque.java
@@ -0,0 +1,20 @@
+public class MyDeque<E>{
+  private E[] data;
+  private int size;
+  private int start, end;
+  public MyDeque(){
+    @SuppressWarnings("unchecked")
+    E[] d = (E[])new Object[10];
+    data = d;
+  }
+  public MyDeque(){  }
+  public MyDeque(int initialCapacity){  }
+  public int size(){ }
+  public String toString(){ }
+  public void addFirst(E element){ }
+  public void addLast(E element){ }
+  public E removeFirst(){ }
+  public E removeLast(){ }
+  public E getFirst(){ }
+  public E getLast(){ }
+}
