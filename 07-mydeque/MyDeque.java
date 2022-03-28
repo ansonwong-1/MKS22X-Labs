9128373044d1e2c690dae644c82a89ad99a3f8f7
Anson <awong30@stuy.edu>
2022-03-28 11:41:59 -0400

    toString that prints in right order

--- 07-mydeque/MyDeque.java
+++ 07-mydeque/MyDeque.java
@@ -13,15 +13,38 @@ public class MyDeque<E>{
   }
 
   private void resize(){
-
+    E[] d = (E[]) new Object[not sure what number];
+    int s = start;
+    for (int i = 0; i < size; i++){
+      if (s == data.length){
+        s = 0;
+      }
+      if (s < data.length){
+        d[i] = data[s];
+        s++;
+      }
+    }
   }
-  public MyDeque(){  }
 
   public MyDeque(int initialCapacity){  }
 
   // public int size(){ }
-  //
   public String toString(){
+    String ans = '[';
+    int s = start;
+    for (int i = 0; i < size; i++){
+      if (s == data.length){
+        s = 0;
+      }
+      ans += data[s];
+      s++;
+      if (i != size - 1){
+        ans += ", ";
+      }
+    }
+    return ans + "]";
+  }
+  public String toStringAll(){
     // toString format has comma space between values, examples: "[a, b, c, d]" "[]" "[VALUE, VALUE2, VALUE3]"
     String ans = "[";
     for (int i = 0 ; i < size; i++){
