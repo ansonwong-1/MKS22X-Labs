4f1b7e3008eeb796bcf2b5216e88456079128dff
Anson <awong30@stuy.edu>
2022-03-29 16:51:09 -0400

    addfirst maybe

--- 07-mydeque/Driver.java
+++ 07-mydeque/Driver.java
@@ -1,5 +1,13 @@
 import java.util.*;
 import java.io.*;
 public class Driver{
-
+  public static void main (String[] args){
+    MyDeque st = new MyDeque();
+    char add = 'a';
+    for(int i = 0; i < 26; i++){
+      st.addFirst(add);
+      add++;
+      System.out.println(st.toStringAll());
+    }
+  }
 }
--- 07-mydeque/MyDeque.java
+++ 07-mydeque/MyDeque.java
@@ -15,23 +15,32 @@ public class MyDeque<E>{
     size = 0;
   }
 
-  // private void resize(){
-  //   if (this.)
-  //   E[] d = (E[]) new Object[size * 2 + 1];
-  //   int s = start;
-  //   for (int i = 0; i < size; i++){
-  //     if (s == data.length){
-  //       s = 0;
-  //     }
-  //     if (s < data.length){
-  //       d[i] = data[s];
-  //       s++;
-  //     }
-  //   }
-  // }
+  private void resize(){
+    if (size == data.length - 1){
+      E[] d = (E[]) new Object[size * 2 + 1];
+      int s = start;
+      for (int i = 0; i < size; i++){
+        if (s == data.length){
+          s = 0;
+        }
+        if (s < data.length){
+          d[i] = data[s];
+          s++;
+        }
+      }
+      data = d;
+      start = 0;
+      end = data.length;
+    }
+  }
 
   public MyDeque(int initialCapacity){
-
+    @SuppressWarnings("unchecked")
+    E[] d = (E[])new Object[initialCapacity];
+    data = d;
+    start = 0;
+    end = -1;
+    size = 0;
   }
 
   public int size(){
@@ -54,7 +63,6 @@ public class MyDeque<E>{
     return ans + "]";
   }
   public String toStringAll(){
-    // toString format has comma space between values, examples: "[a, b, c, d]" "[]" "[VALUE, VALUE2, VALUE3]"
     String ans = "[";
     for (int i = 0 ; i < data.length; i++){
       ans += data[i];
@@ -63,34 +71,43 @@ public class MyDeque<E>{
       }
     }return ans + "]" + '\n' + "start: " + start + "   end: " + end;
   }
-  //
-  // public void addFirst(E element){ }
-  //
+
+  public void addFirst(E element){
+    resize();
+    if (start == 0){
+      start = data.length - 1;
+    }else{
+      start--;
+    }
+    data[start] = element;
+    size++;
+  }
+
   // public void addLast(E element){ }
 
-  public E removeFirst(){
-    if (size > 0){
-      E ans = data[start];
-      data[start] = null;
-      start++;
-      return ans;
-    }else if(size == 0){
-      throw new NoSuchElementException ("no elements in the deque");
-    }else{
-      return null;
-    }
-  }
-
-  public E removeLast(){
-    if (size > 0){
-      E ans = data[end];
-      data[end] = null;
-      end--;
-      return ans;
-    }else if (size == 0){
-      throw new NoSuchElementException ("no elements in the deque");
-    }
-  }
+  // public E removeFirst(){
+  //   if (size > 0){
+  //     E ans = data[start];
+  //     data[start] = null;
+  //     start++;
+  //     return ans;
+  //   }else if(size == 0){
+  //     throw new NoSuchElementException ("no elements in the deque");
+  //   }else{
+  //     return null;
+  //   }
+  // }
+  //
+  // public E removeLast(){
+  //   if (size > 0){
+  //     E ans = data[end];
+  //     data[end] = null;
+  //     end--;
+  //     return ans;
+  //   }else if (size == 0){
+  //     throw new NoSuchElementException ("no elements in the deque");
+  //   }
+  // }
 
   // public E getFirst(){
   //
