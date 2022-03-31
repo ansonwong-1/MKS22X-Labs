d79a1057d56c28806de23ae91e768f6b8f2399e8
Anson <awong30@stuy.edu>
2022-03-31 17:05:44 -0400

    changed to scanner & added operations

--- 08-calculator/Calculator.java
+++ 08-calculator/Calculator.java
@@ -14,29 +14,51 @@ public class Calculator{
 // eval("8 2 + 99 9 - * 2 + 9 -") is 893.0
 // eval("1 2 3 4 5 + * - -") is 26.0
   public static double eval(String s){
-    Deque<Double> operands = new ArrayDeque<Double>();
-    ArrayList<Character> operators = new ArrayList<Character>();
-    for (int i = s.length() - 1; i > 0; i--){
-      String num = "";
-      char c = s.charAt(i);
-      if (c == '+' || c == '-' || c == '/' || c == '*' || c == '%'){
-        operators.add(c);
-      }else if (c == ' '){
-        i--;
+    ArrayDeque<Double> nums = new ArrayDeque<Double>();
+    Scanner in = new Scanner(s);
+    while (in.hasNext()){
+      if (in.hasNextDouble()){
+        nums.addFirst(in.nextDouble());
       }else{
-        while (s.charAt(i) != ' '){
-          num+= s.charAt(i);
-          i--;
-        }operands.addFirst(Double.parseDouble(num));
+        if (nums.size() <= 1){
+          throw new IllegalArgumentException("too few operands for operation" + in.next());
+        }
+        double n = nums.removeFirst();
+        String i = in.next();
+        if (i.equals("+")){
+          nums.addFirst(nums.removeFirst() + n);
+        }
+        if (i.equals("-")){
+          nums.addFirst(nums.removeFirst() - n);
+        }
+        if (i.equals("*")){
+          nums.addFirst(nums.removeFirst() * n);
+        }
+        if (i.equals("/")){
+          nums.addFirst(nums.removeFirst() / n);
+        }
+        if (i.equals("%")){
+          nums.addFirst(nums.removeFirst() % n);
+        }
       }
-    }
-    if (operators.size() < operands.size() - 1){
+    }if (nums.size() > 1){
       throw new IllegalArgumentException("too many operands");
-    }
-    if (operators.size() >= operands.size()){
-      throw new IllegalArgumentException("too few operands for operation" + operators.get(0));
+    }else{
+      return nums.getFirst();
     }
   }
 
-  public static double operation(double num1, double num2, )
+  public static void main (String[] args){
+    System.out.println(eval("4 5 7 2 + - *"));
+    System.out.println(eval("3 4 + 2 * 7 /"));
+    System.out.println(eval("11 3 - 4 + 2.5 *"));
+    System.out.println(eval("10 2.0 +"));
+    System.out.println(eval("8 2 + 99 9 - * 2 + 9 -"));
+    System.out.println(eval("1 2 3 4 5 + * - -"));
+    // eval("11 3 - 4 + 2.5 *") is 30.0
+    // eval("10 2.0 +") is 12.0
+    // eval("8 2 + 99 9 - * 2 + 9 -") is 893.0
+    // eval("1 2 3 4 5 + * - -") is 26.0
+  }
+
 }
