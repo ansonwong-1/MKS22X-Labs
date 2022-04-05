227c9a1d0febace8d0968449e38a1e1566432bf0
Anson <awong30@stuy.edu>
2022-04-05 11:41:12 -0400

    classwork w frontier

---
+++ 09-treeburning/Frontier.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+public class Frontier{
+  private ArrayDeque<int[]> frontier;
+  public Frontier(){frontier = new ArrayDeque<int[]>();}
+  public int size(){return frontier.size();}
+  public void add(int[]location){frontier.addLast(location);}
+  public int[] remove(){return frontier.removeFirst();}
+}
